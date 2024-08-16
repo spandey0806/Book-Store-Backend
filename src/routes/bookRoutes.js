6eaaae7e13d23a6adda9d2883cd12b4921ee7d3c
Shubham Pandey <shubhampandey0806@gmail.com>
2024-08-16 23:43:46 +0530

    added rds-combined file

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,9 +4,9 @@ const bookController = require('../controllers/bookController');
 
 const router = express.Router();
 
-router.get('/', bookController.getBooks); // Assuming getBooks is a function in bookController
-router.post('/',  bookController.createBook); // Using authMiddleware for protected routes
-router.put('/:id',  bookController.updateBook); // Protected route
-router.delete('/:id',   bookController.deleteBook); // Protected route
+router.get('/', bookController.getBooks);  
+router.post('/',  bookController.createBook);  
+router.put('/:id',  bookController.updateBook);  
+router.delete('/:id',   bookController.deleteBook);  
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const routes = require('./routes');
 const { Sequelize } = require('sequelize');
+const cors = require('cors');
+
+const fs = require('fs');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT||3000;
- 
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 app.get("/ping", (req, res) => {
@@ -20,12 +23,19 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    logging: console.log,
+    dialect: 'mysql',
+    "dialectOptions": {
+          "ssl": {
+            rejectUnauthorized: false,
+              "ca": fs.readFileSync('./rds-combined-ca-bundle.pem')
+          }
       }
-  );
+    },);
 
-
+  
 
 
 sequelize.sync().then(() => {
